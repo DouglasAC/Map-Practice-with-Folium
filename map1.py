c4ddb7eac4280a1a06fe03f6f62867893ad0f4df
DouglasAC <3049786550116@ingenieria.usac.edu.gt>
2024-11-13 17:02:17 -0600

    Agregar un marcador

--- map1.py
+++ map1.py
@@ -1,6 +1,10 @@
 import folium
 
-map = folium.Map(location=[14.547, -90.527], zoom_start=25, title="Stamen Terrain")
+map = folium.Map(location=[14.586521217402636, -90.52169884971288], zoom_start=25, title="Stamen Terrain")
 
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[14.586521217402636, -90.52169884971288], popup="Paiz, Américas", icon=folium.Icon(color="green")))
+
+map.add_child(fg)
 
 map.save("Map1.html")
