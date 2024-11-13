a211d17b7083f0e809f4c10b430007ae3745167c
DouglasAC <3049786550116@ingenieria.usac.edu.gt>
2024-11-13 17:46:33 -0600

    Agregando una lista de lugares

--- map1.py
+++ map1.py
@@ -1,8 +1,16 @@
 import folium
+import pandas as pd
 
-map = folium.Map(location=[14.586521217402636, -90.52169884971288], zoom_start=25, title="Stamen Terrain")
+data = pd.read_csv("Lugares.txt")
+
+map = folium.Map(location=[14.586521217402636, -90.52169884971288], zoom_start=15, title="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
+
+for row in data.itertuples(index=False):
+    lugar, latitud, longitud = row.NOMBRE, row.LAT, row.LOG
+    fg.add_child(folium.Marker(location=[latitud, longitud], popup=lugar, icon=folium.Icon(color="blue")))
+
 fg.add_child(folium.Marker(location=[14.586521217402636, -90.52169884971288], popup="Paiz, Américas", icon=folium.Icon(color="green")))
 
 map.add_child(fg)
