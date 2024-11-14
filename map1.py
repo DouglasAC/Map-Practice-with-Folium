c51b8d45493ee3172f7ae25394394c2d54c29277
DouglasAC <3049786550116@ingenieria.usac.edu.gt>
2024-11-13 18:24:06 -0600

    Actualizar el sistema de marcadores en el mapa para usar tipos en lugar de colores y modificar el archivo de datos correspondiente

--- map1.py
+++ map1.py
@@ -1,6 +1,17 @@
 import folium
 import pandas as pd
 
+tipo_color = {
+        "PR": "green",
+        "CC": "blue",
+        "MU": "purple",
+        "UN": "orange",
+        "HO": "red",
+        "AE": "gray",
+        "AV": "cadetblue"
+    }
+    
+
 data = pd.read_csv("Lugares.txt")
 
 map = folium.Map(location=[14.586521217402636, -90.52169884971288], zoom_start=15, title="Stamen Terrain")
@@ -8,9 +19,9 @@ map = folium.Map(location=[14.586521217402636, -90.52169884971288], zoom_start=1
 fg = folium.FeatureGroup(name="My Map")
 
 for row in data.itertuples(index=False):
-    lugar, latitud, longitud, color = row.NOMBRE, row.LAT, row.LOG, str(row.COLOR).strip().lower()
-    fg.add_child(folium.Marker(location=[latitud, longitud], popup=lugar, icon=folium.Icon(color=color)))
-
+    lugar, latitud, longitud, tipo = row.NOMBRE, row.LAT, row.LOG, str(row.TIPO).strip()
+    fg.add_child(folium.Marker(location=[latitud, longitud], popup=lugar, icon=folium.Icon(color=tipo_color[tipo])))
+    
 
 map.add_child(fg)
 
